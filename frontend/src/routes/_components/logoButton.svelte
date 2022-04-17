4430067875e89f07aaaa853a2c43be2ebad8b86a
Junjie Wang <microdou@gmail.com>
2022-04-17 14:36:07 -0400

    add logo icon

--- frontend/src/routes/_components/logoButton.svelte
+++ frontend/src/routes/_components/logoButton.svelte
@@ -1,5 +1,10 @@
 <script>
   import { Button } from "@svelteness/kit-docs";
+  import DocStackIcon from "~icons/system-uicons/document-stack";
 </script>
 
-<Button href="/">Chait Lab Docs</Button>
+<Button href="/">
+  <div class="tw-flex tw-items-center tw-gap-1 tw-text-teal-500">
+    <DocStackIcon width="24" height="24" /><span>Chait Lab Docs</span></div
+  >
+</Button>
